5ef5bc7bfb6b690708863c96beda31fcaa6a9f19
Chan <ieejo716@naver.com>
2024-06-23 17:57:42 +0900

    [FIN] - 구현

---
+++ src/main/java/org/example/book/implementation/시각.java
@@ -0,0 +1,33 @@
+package org.example.book.implementation;
+
+public class 시각 {
+    public static void main(String[] args) {
+        //input
+        int N = 5;
+        solution(N);
+    }
+
+    public static void solution(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        int h = 0, m = 0, s = 0;
+        int count = 0;
+        while (h != n + 1) {
+            if (isContain(h + "" + m + s)) ++count;
+            if (++s == 60) {
+                s = 0;
+                ++m;
+            }
+            if (m == 60) {
+                m = 0;
+                ++h;
+            }
+        }
+        sb.append(count);
+        System.out.println(sb);
+    }
+
+    private static boolean isContain(String s) {
+        return s.contains("3");
+    }
+}
---
+++ src/main/java/org/example/book/implementation/왕실의나이트.java
@@ -0,0 +1,37 @@
+package org.example.book.implementation;
+
+public class 왕실의나이트 {
+    public static void main(String[] args) {
+        //input
+        String i1 = "a1";
+        String i2 = "c2";
+        String[] idx = {"a1", "c2"};
+        for (String i : idx) {
+            solution(i);
+        }
+    }
+
+    public static void solution(String index) {
+        StringBuilder sb = new StringBuilder();
+        char[] idx = index.toCharArray();
+        int y = idx[0] - 96;
+        int x = idx[1] - 48;
+        int[][] move = {
+                {-2, -1}, {-2, 1},
+                {2, -1}, {2, 1},
+                {-1, -2}, {-1, 2},
+                {1, -2}, {1, 2}
+        };
+        int answer = 0;
+        for (int[] m : move) {
+            if (outOfRange(y + m[0], x + m[1])) continue;
+            ++answer;
+        }
+        sb.append(answer);
+        System.out.println(answer);
+    }
+
+    private static boolean outOfRange(int y, int x) {
+        return y < 1 || x < 1 || y > 8 || x > 8;
+    }
+}
