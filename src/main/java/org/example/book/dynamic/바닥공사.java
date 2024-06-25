0368cb0a5dc7dbf66779b4094dcaa47effe4b186
Chan <ieejo716@naver.com>
2024-06-25 17:16:22 +0900

    [FIN] - DP

--- src/main/java/org/example/book/dynamic/바닥공사.java
+++ src/main/java/org/example/book/dynamic/바닥공사.java
@@ -22,7 +22,7 @@ public class 바닥공사 {
         dp[1] = 1;
         dp[2] = 3;
         for (int i = 3; i <= N; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] * 2;
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 796796;
         }
         sb.append(dp[N]);
     }
