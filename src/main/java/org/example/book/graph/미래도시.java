47f0c2f7991c4384e7351aec2f64b14eea26ca1b
Chan <ieejo716@naver.com>
2024-07-25 16:43:20 +0900

    [FIN] - 플로이드 워셜

---
+++ src/main/java/org/example/book/graph/미래도시.java
@@ -0,0 +1,81 @@
+package org.example.book.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+5 7
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+4 5
+4 5
+답 : 3
+ */
+/*
+4 2
+1 3
+2 4
+3 4
+답 : -1
+ */
+public class 미래도시 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N, M, X, K;
+    static int[][] graph;
+    static final int INF = 100_000_000;
+
+    public static void main(String[] args) throws IOException {
+        inputs();
+        solution();
+        output();
+    }
+
+    private static void solution() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+    }
+
+    private static void inputs() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i != j) graph[i][j] = INF;
+            }
+        }
+
+        for (int k = 0; k < M; k++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = 1;
+            graph[b][a] = 1;
+        }
+
+        st = new StringTokenizer(br.readLine());
+        X = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+    }
+
+    private static void output() {
+        int dist = graph[1][K] + graph[K][X];
+        sb.append(dist >= INF ? -1 : dist);
+        System.out.println(sb);
+    }
+}
