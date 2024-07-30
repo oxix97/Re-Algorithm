24758eac91e2caf5652a09537d1a7c4cb47349f2
Chan <ieejo716@naver.com>
2024-07-30 15:28:02 +0900

    [FIN] - 문자열 뒤집기 : 그리디

---
+++ src/main/java/org/example/book/greedy/문자열뒤집기.java
@@ -0,0 +1,43 @@
+package org.example.book.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 문자열뒤집기 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        arr = Arrays.stream(br.readLine().split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        //0으로 만들 때
+        int zero = checkNumber(0);
+
+        //1로 만들 때
+        int one = checkNumber(1);
+
+        int answer = Math.min(zero, one);
+        System.out.println(answer);
+    }
+
+    private static int checkNumber(int target) {
+        int count = 0;
+        boolean check = false;
+        for (int n : arr) {
+            if (n == target) {
+                if (!check) {
+                    check = true;
+                    ++count;
+                }
+            } else {
+                check = false;
+            }
+        }
+        return count;
+    }
+
+}
