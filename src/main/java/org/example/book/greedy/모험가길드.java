82a70c01c3b4fa2ca51754c1d8a59efe99720e36
Chan <ieejo716@naver.com>
2024-07-30 15:28:48 +0900

    [FIN] - 모험가 길드 : 그리디

---
+++ src/main/java/org/example/book/greedy/모험가길드.java
@@ -0,0 +1,42 @@
+package org.example.book.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 모험가길드 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        inputs();
+        solution();
+    }
+
+    private static void solution() {
+        Arrays.sort(arr);
+        int answer = 0;
+        int tmp = 0;
+        for (int n : arr) {
+            if (n == ++tmp) {
+                tmp = 0;
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static void inputs() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
