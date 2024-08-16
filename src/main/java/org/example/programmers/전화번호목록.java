a87cd3f2d8608f3477eb55aa4e98a8d69ddb82b1
Chan <ieejo716@naver.com>
2024-08-16 15:26:35 +0900

    [FIN] - 전화번호 목록 : Lv2, 해시

--- src/main/java/org/example/programmers/전화번호목록.java
+++ src/main/java/org/example/programmers/전화번호목록.java
@@ -1,14 +1,19 @@
 package org.example.programmers;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class 전화번호목록 {
     public static boolean solution(String[] phone_book) {
         Arrays.sort(phone_book, (o1, o2) -> o1.length() - o2.length());
-        for (int i = 0; i < phone_book.length; i++) {
-            for (int j = phone_book.length - 1; j > i; j--) {
-                if (phone_book[j].startsWith(phone_book[i])) return false;
+        Set<String> set = new HashSet<>();
+        for (String phone : phone_book) {
+            for (int i = 1; i <= phone.length(); i++) {
+                if (set.contains(phone.substring(0, i)))
+                    return false;
             }
+            set.add(phone);
         }
 
         return true;
@@ -18,7 +23,7 @@ public class 전화번호목록 {
         String[][] phone_books = {
                 {"119", "97674223", "1195524421"},
                 {"123", "456", "789"},
-                {"12", "123", "1235", "567", "88"}
+                {"1235", "123", "12", "567", "88"}
         };
         for (String[] phone_book : phone_books) {
             boolean answer = solution(phone_book);
