a9f3ca6af0c392a498bc238b92eaa5e668bf5036
Chan <ieejo716@naver.com>
2025-05-20 23:54:40 +0900

    파이썬 코테 준비 45일차

--- pythonAlgorithm/baekjoon/흩날리는시험지속에서내평점이느껴진거야_17951.py
+++ pythonAlgorithm/baekjoon/흩날리는시험지속에서내평점이느껴진거야_17951.py
@@ -1,12 +1,11 @@
 import sys
-from collections import deque
 
 if __name__ == '__main__':
     input = sys.stdin.readline
     N, K = map(int, input().split())
     points = list(map(int, input().split()))
     left = 0
-    right = 20 * 10 ** 5
+    right = 1e9
     answer = 0
     while left <= right:
         mid = (left + right) // 2
@@ -18,8 +17,6 @@ if __name__ == '__main__':
                 count += 1
                 sum_point = 0
 
-        if sum_point != 0:
-            count += 1
         if count >= K:
             answer = mid
             left = mid + 1
