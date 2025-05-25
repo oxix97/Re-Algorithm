8f90c3efadede97105ff7c98ad372471eeb7b8fe
Chan <ieejo716@naver.com>
2025-05-25 21:19:08 +0900

    파이썬 코테 준비 47일차

--- pythonAlgorithm/baekjoon/반복하지않는수_7696.py
+++ pythonAlgorithm/baekjoon/반복하지않는수_7696.py
@@ -1,38 +1,27 @@
 # 시간초과
 def main():
-    # 반복하지 않는 수를 미리 계산하여 저장
-    non_repeating_numbers = []
-    num = 1
-
-    # 100만까지의 반복하지 않는 수를 모두 계산 (충분한 양)
-    while len(non_repeating_numbers) < 1000000:
-        # 주어진 코드 방식을 활용하여 반복하지 않는 수인지 확인
-        check = [False] * 10
-        temp_num = num
-        is_non_repeating = True
-
-        while temp_num > 0:
-            tmp = temp_num % 10
-            if check[tmp]:
-                is_non_repeating = False
+    A = []
+    i = 1
+    while len(A) <= 1_000_000:
+        visited = [False] * 10
+        repeatable = False
+        tmp = i
+        while tmp > 0:
+            if visited[tmp % 10]:
+                repeatable = True
                 break
-            check[tmp] = True
-            temp_num //= 10
+            visited[tmp % 10] = True
+            tmp //= 10
+        if not repeatable:
+            A.append(i)
+        i += 1
 
-        # 반복하지 않는 수라면 목록에 추가
-        if is_non_repeating:
-            non_repeating_numbers.append(num)
-
-        num += 1
-
-    # 사용자 쿼리 처리
     while True:
-        n = int(input())
-        if n == 0:
+        N = int(input())
+        if N == 0:
             break
-        # n번째 반복하지 않는 수 출력
-        print(non_repeating_numbers[n - 1])
 
+        print(A[N - 1])
 
 if __name__ == '__main__':
     main()
--- pythonAlgorithm/baekjoon/보석도둑_1202.py
+++ pythonAlgorithm/baekjoon/보석도둑_1202.py
@@ -1,27 +1,27 @@
-import sys
-import heapq
+import sys,heapq
 
-if __name__ == '__main__':
+
+def main():
     input = sys.stdin.readline
     N, K = map(int, input().split())
-    J = []
-    B = []
-    for _ in range(N):
-        m, v = map(int, input().split())
-        J.append((m, v))
+    J = [tuple(map(int,input().split())) for _ in range(N)]
+    B = [int(input()) for _ in range(K)]
+
     J.sort()
-    for _ in range(K):
-        B.append(int(input()))
     B.sort()
+    answer = 0
 
+    pq = []
     idx = 0
-    total_price = 0
-    max_heap = []
-    for bag in B:
-        while idx < N and J[idx][0] <= bag:
-            heapq.heappush(max_heap, -J[idx][1])
+    for i in range(K):
+        while idx < N and J[idx][0] <=B[i]:
+            heapq.heappush(pq,-J[idx][1])
             idx += 1
-        if max_heap:
-            total_price += (heapq.heappop(max_heap) * -1)
 
-    print(total_price)
+        if len(pq) > 0:
+            answer += -heapq.heappop(pq)
+
+    print(answer)
+
+if __name__ == '__main__':
+    main()
